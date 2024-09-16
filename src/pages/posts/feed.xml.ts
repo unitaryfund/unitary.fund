08d9f566430898b5a313bf7198be491b8ba1b9ed
Alessandro Cosentino <alessandro@unitary.fund>
2024-09-16 09:47:10 +0200

    Add an RSS feed
    Partially solves #49 for blog posts

---
+++ src/pages/posts/feed.xml.ts
@@ -0,0 +1,34 @@
+import rss from '@astrojs/rss';
+import { CollectionEntry, getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+interface SiteContext {
+  site: string;
+}
+
+const parser = new MarkdownIt();
+
+function getDateFromPost(post: CollectionEntry<"blog">): Date {
+  return new Date(`${post.data.year}-${post.data.month}-${post.data.day}`);
+}
+
+export async function get(context: SiteContext) {
+  const posts = await getCollection('blog');
+  // Sort the posts by date in descending order
+  const sortedPosts = posts.sort((a, b) => {
+    return getDateFromPost(b).getTime() - getDateFromPost(a).getTime();
+  });
+  return rss({
+    title: 'Unitary Fund blog',
+    description: 'Unitary Fund blog',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      link: `/posts/${post.slug}/`,
+      pubDate: getDateFromPost(post),
+      content: sanitizeHtml(parser.render(post.body)),
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
